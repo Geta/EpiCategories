142c18aa2ab762d17fdf01096dacc9f8e4d02816
Mattias Olsson <mattias@geta.no>
2017-03-02 11:07:25 +0100

    Fixed strange issue with SearchRoots only containing "For All Sites" in search providers.

--- src/EpiCategories.Find/FindCategoriesSearchProvider.cs
+++ src/EpiCategories.Find/FindCategoriesSearchProvider.cs
@@ -1,4 +1,6 @@
-﻿using EPiServer.Core;
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.Find.Cms.SearchProviders;
 using EPiServer.Framework.Localization;
@@ -13,11 +15,13 @@ namespace Geta.EpiCategories.Find
     {
         private readonly LocalizationService _localizationService;
         private readonly ISiteDefinitionResolver _siteDefinitionResolver;
+        private readonly IEnumerable<IContentRepositoryDescriptor> _contentRepositoryDescriptors;
 
-        public FindCategoriesSearchProvider(LocalizationService localizationService, ISiteDefinitionResolver siteDefinitionResolver, IContentTypeRepository contentTypeRepository, UIDescriptorRegistry uiDescriptorRegistry) : base(localizationService, siteDefinitionResolver, contentTypeRepository, uiDescriptorRegistry)
+        public FindCategoriesSearchProvider(LocalizationService localizationService, ISiteDefinitionResolver siteDefinitionResolver, IContentTypeRepository contentTypeRepository, UIDescriptorRegistry uiDescriptorRegistry, IEnumerable<IContentRepositoryDescriptor> contentRepositoryDescriptors) : base(localizationService, siteDefinitionResolver, contentTypeRepository, uiDescriptorRegistry)
         {
             _localizationService = localizationService;
             _siteDefinitionResolver = siteDefinitionResolver;
+            _contentRepositoryDescriptors = contentRepositoryDescriptors;
         }
 
         protected override string ToolTipResourceKeyBase => "/shell/cms/search/pages/tooltip";
@@ -28,6 +32,16 @@ namespace Geta.EpiCategories.Find
 
         public override string Category => _localizationService.GetString("/cms/searchprovider/findcategories/name", "Find categories");
 
+        public override IEnumerable<SearchResult> Search(Query query)
+        {
+            query.SearchRoots = _contentRepositoryDescriptors
+                .First(x => x.Key == CategoryContentRepositoryDescriptor.RepositoryKey)
+                .Roots
+                .Select(x => x.ToReferenceWithoutVersion().ToString());
+
+            return base.Search(query);
+        }
+
         protected override string GetEditUrl(CategoryData contentData, out bool onCurrentHost)
         {
             ContentReference contentLink = contentData.ContentLink;
@@ -35,9 +49,9 @@ namespace Geta.EpiCategories.Find
             string editPath = EditPath(contentData, contentLink, language);
             onCurrentHost = true;
 
-            var contentSiteDefinition = _siteDefinitionResolver.GetByContent(contentLink, true);
+            var contentSiteDefinition = _siteDefinitionResolver.GetByContent(contentLink, true, true);
 
-            if (contentSiteDefinition != null && contentSiteDefinition.SiteUrl == SiteDefinition.Current.SiteUrl)
+            if (contentSiteDefinition.SiteUrl == SiteDefinition.Current.SiteUrl)
                 return editPath;
 
             onCurrentHost = false;
--- src/EpiCategories/Search/CategoriesSearchProvider.cs
+++ src/EpiCategories/Search/CategoriesSearchProvider.cs
@@ -1,6 +1,7 @@
-﻿using EPiServer;
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer;
 using EPiServer.Cms.Shell.Search;
-using EPiServer.Cms.Shell.Search.Internal;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.Framework.Localization;
@@ -18,10 +19,12 @@ namespace Geta.EpiCategories.Search
     public class CategoriesSearchProvider : EPiServerSearchProviderBase<CategoryData, ContentType>
     {
         private readonly LocalizationService _localizationService;
+        private readonly IEnumerable<IContentRepositoryDescriptor> _contentRepositoryDescriptors;
 
-        public CategoriesSearchProvider(LocalizationService localizationService, ISiteDefinitionResolver siteDefinitionResolver, IContentTypeRepository<ContentType> contentTypeRepository, EditUrlResolver editUrlResolver, ServiceAccessor<SiteDefinition> currentSiteDefinition, IContentRepository contentRepository, ILanguageBranchRepository languageBranchRepository, SearchHandler searchHandler, ContentSearchHandler contentSearchHandler, SearchIndexConfig searchIndexConfig, UIDescriptorRegistry uiDescriptorRegistry, LanguageResolver languageResolver, UrlResolver urlResolver, TemplateResolver templateResolver) : base(localizationService, siteDefinitionResolver, contentTypeRepository, editUrlResolver, currentSiteDefinition, contentRepository, languageBranchRepository, searchHandler, contentSearchHandler, searchIndexConfig, uiDescriptorRegistry, languageResolver, urlResolver, templateResolver)
+        public CategoriesSearchProvider(LocalizationService localizationService, ISiteDefinitionResolver siteDefinitionResolver, IContentTypeRepository<ContentType> contentTypeRepository, EditUrlResolver editUrlResolver, ServiceAccessor<SiteDefinition> currentSiteDefinition, IContentRepository contentRepository, ILanguageBranchRepository languageBranchRepository, SearchHandler searchHandler, ContentSearchHandler contentSearchHandler, SearchIndexConfig searchIndexConfig, UIDescriptorRegistry uiDescriptorRegistry, LanguageResolver languageResolver, UrlResolver urlResolver, TemplateResolver templateResolver, IEnumerable<IContentRepositoryDescriptor> contentRepositoryDescriptors) : base(localizationService, siteDefinitionResolver, contentTypeRepository, editUrlResolver, currentSiteDefinition, contentRepository, languageBranchRepository, searchHandler, contentSearchHandler, searchIndexConfig, uiDescriptorRegistry, languageResolver, urlResolver, templateResolver)
         {
             _localizationService = localizationService;
+            _contentRepositoryDescriptors = contentRepositoryDescriptors;
         }
 
         public override string Area => "CMS/categories";
@@ -29,5 +32,15 @@ namespace Geta.EpiCategories.Search
         public override string Category => _localizationService.GetString("/cms/searchprovider/categories/name", "Categories");
 
         protected override string IconCssClass => "epi-resourceIcon epi-resourceIcon-category";
+
+        public override IEnumerable<SearchResult> Search(Query query)
+        {
+            query.SearchRoots = _contentRepositoryDescriptors
+                .First(x => x.Key == CategoryContentRepositoryDescriptor.RepositoryKey)
+                .Roots
+                .Select(x => x.ToReferenceWithoutVersion().ToString());
+
+            return base.Search(query);
+        }
     }
 }
