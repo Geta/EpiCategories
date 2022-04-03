696b828adda05b9a92def909bdff07451b322160
Brian Weeteling <brianweeteling@gmail.com>
2022-04-03 12:02:29 +0200

    Add example: controller and view for categories

---
+++ tests/EpiCategories.SandboxApp/Controllers/BasicCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using EpiCategories.SandboxApp.Models.Categories;
+using EPiServer.Core;
+using EPiServer.Web.Mvc;
+
+namespace EpiCategories.SandboxApp.Controllers
+{
+    public class BasicCategoryController : ContentController<BasicCategory>
+    {
+        public ActionResult Index(BasicCategory currentContent, int page = 1)
+        {
+            if (currentContent.IsOverview)
+            {
+                return View(currentContent);
+            }
+
+            return View(currentContent);
+        }
+    }
+}
---
+++ tests/EpiCategories.SandboxApp/Models/Categories/BasicCategory.cs
@@ -0,0 +1,11 @@
+﻿using EPiServer.DataAnnotations;
+using Geta.EpiCategories;
+
+namespace EpiCategories.SandboxApp.Models.Categories
+{
+    [ContentType]
+    public class BasicCategory : CategoryData
+    {
+        public virtual bool IsOverview { get; set; }
+    }
+}
